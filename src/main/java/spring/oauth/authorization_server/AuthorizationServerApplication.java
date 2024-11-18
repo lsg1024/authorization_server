0a355a06ee56617d140821386da1753c44c9994b
JS <zks145@naver.com>
2024-11-18 14:53:46 +0900

    fix: api-key properties 읽기 문제 해결

--- src/main/java/spring/oauth/authorization_server/AuthorizationServerApplication.java
+++ src/main/java/spring/oauth/authorization_server/AuthorizationServerApplication.java
@@ -2,8 +2,10 @@ package spring.oauth.authorization_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secret-api-key.properties")
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
